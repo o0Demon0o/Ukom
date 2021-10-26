e8b7b53228c72dfeb5cb0e38d94e58b122a7dd63
Prima Mahardika <68330230+o0Demon0o@users.noreply.github.com>
2021-10-26 13:02:54 +0700

    Ukom

---
+++ JubitorRemake/app/src/main/java/com/example/jubitorremake/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.jubitorremake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    Button explicit_btn, implicit_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void MethodMasuk(View view) {
+        Intent intent = new  Intent(getBaseContext(), masuk.class);
+        startActivity(intent);
+    }
+
+    public void MethodDaftar(View view) {
+        Intent intent = new  Intent(getBaseContext(), daftar.class);
+        startActivity(intent);
+    }
+}
---
+++ JubitorRemake/app/src/main/java/com/example/jubitorremake/daftar.java
@@ -0,0 +1,14 @@
+package com.example.jubitorremake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class daftar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daftar);
+    }
+}
